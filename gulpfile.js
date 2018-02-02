be255a149da967f2d04e63a7ad38ec40e33e7a39
Aditya Purwa <adityapurwa@windowslive.com>
2018-02-02 21:38:46 +0700

    update jquery version, change package.json, change build flow, change example

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ var typescript = require('gulp-typescript');
 gulp.task('default', function () {
     gulp.src(['src/**/*.ts'])
         .pipe(typescript())
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest('example'));
 });
 
 gulp.watch(['src/**/*.ts'], ['default']);
